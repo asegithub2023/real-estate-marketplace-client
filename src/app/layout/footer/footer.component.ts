d64c93f3a5a8e2872522d202fd7a279b2fca1df6
amikije <mahirjelalu@gmail.com>
2026-08-17 05:46:37 -0700

    feat: enhance footer with navigation links and social media

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,4 +8,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+}
